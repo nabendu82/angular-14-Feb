e0bec303bb8a64bb72ab71fee43a09176e054f6d
nabendu82 <nabendu.biswas@gmail.com>
2022-02-14 21:50:25 +0530

    Final commit

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: '.app-servers',
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>
+  // `,
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverStatus = 'No server was created';
+  serverName = '';
+  serverCreated = false;
+  servers = ['SR1', 'SR2', 'SR3'];
+
+  constructor() { 
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  ngOnInit(): void {
+  }
+
+  onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverStatus = `Server created - ${this.serverName}`;
+  }
+
+  onUpdate(event: any){
+    this.serverName = event.target.value;
+  }
+
+}
